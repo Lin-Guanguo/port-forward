687a64005c658a546370eca31ee43ef454750207
Lin-Guanguo <lin.guanguo2000@gmail.com>
2022-01-17 22:18:29 +0800

    Server data struct

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,44 @@
-struct Server {}
+#[derive(Debug)]
+struct User {
+    uuid: uuid::Uuid,
+    ports: Vec<(i32, i32)>,
+}
+
+impl User {
+    fn new(uuid: uuid::Uuid) -> Self {
+        User {
+            uuid,
+            ports: Vec::new(),
+        }
+    }
+
+    fn add_port(&mut self, client_port: i32, server_port: i32) {
+        self.ports.push((client_port, server_port));
+    }
+}
+
+#[derive(Debug)]
+struct Server {
+    users: Vec<User>,
+}
+
+impl Server {
+    fn new() -> Server {
+        Server { users: Vec::new() }
+    }
+
+    fn add_user(&mut self, user: User) {
+        self.users.push(user);
+    }
+
+    fn run(&mut self) -> anyhow::Result<()> {
+        Ok(())
+    }
+}
 
 fn main() -> Result<(), uuid::Error> {
+    let mut server = Server::new();
+
     let lua = rlua::Lua::new();
     let lua_ret = lua.context(|ctx| {
         let config_file = std::fs::read_to_string("server-config.lua")?;
@@ -9,19 +47,28 @@ fn main() -> Result<(), uuid::Error> {
         let config: rlua::Table = g.get("config")?;
         let users: rlua::Table = config.get("users")?;
         for i in 0..users.len()? {
-            let user: rlua::Table = users.get(i + 1)?;
-            let uuid: String = user.get("uuid")?;
-            let ports_table: rlua::Table = user.get("ports")?;
-            let mut ports: Vec<String> = vec![];
-            for i in 0..ports_table.len()? {
-                ports.push(ports_table.get(i + 1)?)
+            let user_tbl: rlua::Table = users.get(i + 1)?;
+            let uuid: String = user_tbl.get("uuid")?;
+            let ports_tbl: rlua::Table = user_tbl.get("ports")?;
+
+            let mut user = User::new(uuid::Uuid::parse_str(&uuid)?);
+            for i in 0..ports_tbl.len()? {
+                let s: String = ports_tbl.get(i + 1)?;
+                let mut s = s.split(":");
+                user.add_port(s.next().unwrap().parse()?, s.next().unwrap().parse()?)
             }
-            println!("uuid = {:?}", uuid);
-            println!("ports = {:?}", ports);
+            (&mut server).add_user(user);
         }
         anyhow::Ok(())
     });
-    lua_ret.unwrap();
+    if let Err(e) = lua_ret {
+        panic!("lua config file error {}", e);
+    }
+
+    println!("server {:?}", server);
+    if let Err(e) = server.run() {
+        panic!("server run error {}", e);
+    }
 
     Ok(())
 }
