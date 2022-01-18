103336f4513c429e64a803e68ae4aff91f335489
Lin-Guanguo <lin.guanguo2000@gmail.com>
2022-01-18 11:14:55 +0800

    handle_connection 框架

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,20 @@
-use tokio::io::{AsyncRead, AsyncWrite};
+use std::collections::{HashMap, HashSet};
+use std::fmt::Display;
+use std::sync::{Arc, Mutex};
+use thiserror::Error;
+
+use tokio::io::{AsyncRead, AsyncReadExt, AsyncWrite, AsyncWriteExt};
 use tokio::net::{TcpListener, TcpStream};
+use uuid::Uuid;
 
 #[derive(Debug)]
 struct User {
-    uuid: uuid::Uuid,
+    uuid: Uuid,
     ports: Vec<(i32, i32)>,
 }
 
 impl User {
-    fn new(uuid: uuid::Uuid) -> Self {
+    fn new(uuid: Uuid) -> Self {
         User {
             uuid,
             ports: Vec::new(),
@@ -20,23 +26,60 @@ impl User {
     }
 }
 
+#[derive(Debug, Error)]
+enum Error {
+    #[error("duplicate conncetion with same uuid")]
+    ConnectionDuplicate(Uuid),
+
+    #[error("unkonw user")]
+    UnkonwUser(Uuid),
+}
+
 #[derive(Debug)]
 struct Server {
-    users: Vec<User>,
+    users: HashMap<Uuid, User>,
+    online_users: Mutex<HashSet<Uuid>>,
 }
 
 impl Server {
     fn new() -> Server {
-        Server { users: Vec::new() }
+        Server {
+            users: HashMap::new(),
+            online_users: Mutex::new(HashSet::new()),
+        }
     }
 
     fn add_user(&mut self, user: User) {
-        self.users.push(user);
+        self.users.insert(user.uuid.clone(), user);
     }
 
-    async fn run(&mut self) -> anyhow::Result<()> {
+    async fn run(&'static self) -> anyhow::Result<()> {
         let main_listener = Server::listen(8077).await?;
-        Ok(())
+        loop {
+            let (connection, addr) = main_listener.accept().await?;
+            println!("connetion from {:?}", addr);
+            let _ = tokio::spawn(self.handle_connection(connection));
+        }
+    }
+
+    async fn handle_connection(&self, mut connection: TcpStream) -> anyhow::Result<()> {
+        let mut uuid = [0u8; 16];
+        connection.read_exact(&mut uuid).await?;
+        let uuid = Uuid::from_bytes(uuid);
+
+        let not_existing = self.online_users.lock().unwrap().insert(uuid);
+        if !not_existing {
+            let r = [0u8];
+            connection.write_all(&r).await?;
+            return Err(Error::ConnectionDuplicate(uuid).into());
+        }
+
+        if let Some(user) = self.users.get(&uuid) {
+            user.ports.iter().map(|(client_port, server_port)| todo!());
+            Ok(())
+        } else {
+            Err(Error::UnkonwUser(uuid).into())
+        }
     }
 
     async fn listen(port: i32) -> tokio::io::Result<TcpListener> {
@@ -45,8 +88,8 @@ impl Server {
 }
 
 #[tokio::main]
-async fn main() -> Result<(), uuid::Error> {
-    let mut server = Server::new();
+async fn main() {
+    let mut server = Box::new(Server::new());
 
     let lua = rlua::Lua::new();
     let lua_ret = lua.context(|ctx| {
@@ -60,13 +103,13 @@ async fn main() -> Result<(), uuid::Error> {
             let uuid: String = user_tbl.get("uuid")?;
             let ports_tbl: rlua::Table = user_tbl.get("ports")?;
 
-            let mut user = User::new(uuid::Uuid::parse_str(&uuid)?);
+            let mut user = User::new(Uuid::parse_str(&uuid)?);
             for i in 0..ports_tbl.len()? {
                 let s: String = ports_tbl.get(i + 1)?;
                 let mut s = s.split(":");
                 user.add_port(s.next().unwrap().parse()?, s.next().unwrap().parse()?)
             }
-            (&mut server).add_user(user);
+            server.add_user(user);
         }
         anyhow::Ok(())
     });
@@ -74,10 +117,10 @@ async fn main() -> Result<(), uuid::Error> {
         panic!("lua config file error {}", e);
     }
 
+    let server = Box::leak::<'static>(server);
+
     println!("server {:?}", server);
     if let Err(e) = server.run().await {
         panic!("server run error {}", e);
     }
-
-    Ok(())
 }
